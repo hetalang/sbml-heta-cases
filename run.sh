51b3e4513e10bd7083ed84aa623d2e91c702d1d9
Evgeny Metelkin <metelkin@insysbio.com>
2020-08-21 12:43:27 +0300

    manual sim

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 heta build
 export heta_version=`heta -v`
-julia_path="C:/Users/evgen/AppData/Local/Programs/Julia 1.5.0/bin/"
-"${julia_path}julia" ./run.jl $heta_version
-echo "READY!"
+#julia_path="C:/Users/evgen/AppData/Local/Programs/Julia 1.5.0/bin/"
+#"${julia_path}julia" ./run.jl $heta_version
+julia ./run.jl $heta_version
