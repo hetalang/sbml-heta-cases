c9dc8c98899d050195d65179480b459e81fdb06d
Evgeny Metelkin <evgeny.metelkin@gmail.com>
2024-06-03 12:17:45 +0300

    draft l3v2

--- bash/make-description.sh
+++ bash/make-description.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-base_dir="result/l2v5"
+# $1 is version of SBML
+base_dir="result/$1"
 
 echo Create of file structure
 mkdir -p $base_dir
@@ -13,7 +14,6 @@ echo "  \"hetaCompilerVersion\": \"$hetaCompilerVersion\"," >> $base_dir/summary
 started=$(date)
 echo "  \"started\": \"$started\"," >> $base_dir/summary.json
 dirs=$(find ./cases/semantic/ -type d -regex '.*/[0-9]+' -print0 | xargs -0 -n1 basename | sort ) # find all directories with numbers and sort by name
-#dirs=$(find ./cases/semantic/ -type f -name "*-sbml-l2v5.xml" -exec dirname {} \; | xargs -n1 basename )
 totalCasesCount=$(echo "$dirs" | wc -l)
 echo "  \"totalCasesCount\": \"$totalCasesCount\"," >> $base_dir/summary.json
 
@@ -30,13 +30,13 @@ for dir in $dirs; do
     fi
     
     mkdir -p $base_dir/$dir
-    cp bash/index.heta $base_dir/$dir/index.heta
+    cp bash/$1-index.heta $base_dir/$dir/index.heta
 
     # Extract line starting from "synopsis" until the end of line
     synopsis=$(sed -n '/(\*/,/*)/p' cases/semantic/$dir/$dir-model.m | sed '1d;$d')
     echo "$synopsis" > $base_dir/$dir/synopsis.txt
 
-    cp cases/semantic/$dir/$dir-sbml-l2v5.xml $base_dir/$dir/model-sbml-l2v5.xml > /dev/null 2>&1
+    cp cases/semantic/$dir/$dir-sbml-$1.xml $base_dir/$dir/model-sbml-$1.xml > /dev/null 2>&1
     if [ $? -ne 0 ]; then
         echo "    {\"id\": \"$dir\", \"retCode\": 9}$delimiter" >> $base_dir/summary.json
         echo "$dir has no SBML file"
